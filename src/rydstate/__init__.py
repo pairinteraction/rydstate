93987ccea4b8dec2f23c73fe429cb23fda9d5f0a
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-12 09:42:58 +0100

    add RydbergStateAlkalineJJ

--- src/rydstate/__init__.py
+++ src/rydstate/__init__.py
@@ -1,9 +1,10 @@
 from rydstate import angular, radial, species
-from rydstate.rydberg_state import RydbergStateAlkali, RydbergStateAlkalineLS
+from rydstate.rydberg_state import RydbergStateAlkali, RydbergStateAlkalineJJ, RydbergStateAlkalineLS
 from rydstate.units import ureg
 
 __all__ = [
     "RydbergStateAlkali",
+    "RydbergStateAlkalineJJ",
     "RydbergStateAlkalineLS",
     "angular",
     "radial",
