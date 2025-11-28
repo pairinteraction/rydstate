c75b692a6f3ef2f62a442fcfa911c531e15158fe
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-28 01:23:30 +0100

    UNDOME: less warnings

--- src/rydstate/angular/angular_state.py
+++ src/rydstate/angular/angular_state.py
@@ -96,7 +96,7 @@ class AngularState(Generic[_AngularKet]):
                 else:
                     kets.append(scheme_ket)
                     coefficients.append(coeff * scheme_coeff)
-        return AngularState(coefficients, kets, warn_if_not_normalized=abs(self.norm - 1) < 1e-10)
+        return AngularState(coefficients, kets, warn_if_not_normalized=False)
 
     def calc_exp_qn(self, q: AngularMomentumQuantumNumbers) -> float:
         """Calculate the expectation value of a quantum number q.
