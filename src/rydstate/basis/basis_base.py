a74c08d4e86cb41512dbdaef7c84441016103681
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-12-03 18:20:17 +0100

    BasisBase start adding calc_reduced_...

--- src/rydstate/basis/basis_base.py
+++ src/rydstate/basis/basis_base.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from abc import ABC
-from typing import TYPE_CHECKING, get_args
+from typing import TYPE_CHECKING, get_args, overload
 
+import numpy as np
 from typing_extensions import Self
 
 from rydstate.angular.angular_matrix_element import AngularMomentumQuantumNumbers
@@ -10,6 +11,8 @@ from rydstate.species.species_object import SpeciesObject
 
 if TYPE_CHECKING:
     from rydstate.rydberg import RydbergStateMQDT, RydbergStateSQDT
+    from rydstate.rydberg.rydberg_base import RydbergStateBase
+    from rydstate.units import MatrixElementOperator, NDArray, PintArray
 
 
 class BasisBase(ABC):
@@ -46,3 +49,57 @@ class BasisBase(ABC):
         if qn in ["n", "nu", "nu_energy"]:
             return [0 for state in self.states]
         raise ValueError(f"Unknown quantum number {qn}")
+
+    def calc_reduced_overlap(self, other: RydbergStateBase) -> NDArray:
+        """Calculate the reduced overlap <self|other> (ignoring the magnetic quantum number m)."""
+        return np.array([bra.calc_reduced_overlap(other) for bra in self.states])
+
+    def calc_reduced_overlaps(self, other: BasisBase) -> NDArray:
+        """Calculate the reduced overlap <bra|ket> for all states in the bases self and other.
+
+        Returns a numpy array overlaps, where overlaps[i,j] corresponds to the overlap of the
+        i-th state of self and the j-th state of other.
+        """
+        return np.array([[bra.calc_reduced_overlap(ket) for ket in other.states] for bra in self.states])
+
+    @overload
+    def calc_reduced_matrix_element(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: None = None
+    ) -> PintArray: ...
+
+    @overload
+    def calc_reduced_matrix_element(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: str
+    ) -> NDArray: ...
+
+    def calc_reduced_matrix_element(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: str | None = None
+    ) -> PintArray | NDArray:
+        r"""Calculate the reduced matrix element.
+
+        See also ...
+
+        """
+        return np.array([bra.calc_reduced_matrix_element(other, operator, unit=unit) for bra in self.states])
+
+    @overload
+    def calc_reduced_matrix_elements(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: None = None
+    ) -> PintArray: ...
+
+    @overload
+    def calc_reduced_matrix_elements(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: str
+    ) -> NDArray: ...
+
+    def calc_reduced_matrix_elements(
+        self, other: RydbergStateBase, operator: MatrixElementOperator, unit: str | None = None
+    ) -> PintArray | NDArray:
+        r"""Calculate the reduced matrix element.
+
+        See also ...
+
+        """
+        return np.array(
+            [[bra.calc_reduced_matrix_element(ket, operator, unit=unit) for ket in other.states] for bra in self.states]
+        )
