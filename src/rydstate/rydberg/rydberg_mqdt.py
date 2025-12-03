c8328d25ba4107938f7483a2d51243b32e32b47b
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-12-03 12:47:53 +0100

    imporve rydberg_mqdt (rename kets -> sqdt_states

--- src/rydstate/rydberg/rydberg_mqdt.py
+++ src/rydstate/rydberg/rydberg_mqdt.py
@@ -24,36 +24,42 @@ _RydbergState = TypeVar("_RydbergState", bound=RydbergStateSQDT)
 
 class RydbergStateMQDT(Generic[_RydbergState]):
     def __init__(
-        self, coefficients: Sequence[float], kets: Sequence[_RydbergState], *, warn_if_not_normalized: bool = True
+        self,
+        coefficients: Sequence[float],
+        sqdt_states: Sequence[_RydbergState],
+        *,
+        nu_energy: float | None = None,
+        warn_if_not_normalized: bool = True,
     ) -> None:
         self.coefficients = np.array(coefficients)
-        self.kets = kets
+        self.sqdt_states = sqdt_states
+        self.nu_energy = nu_energy
 
-        if len(coefficients) != len(kets):
-            raise ValueError("Length of coefficients and kets must be the same.")
-        if not all(type(ket) is type(kets[0]) for ket in kets):
-            raise ValueError("All kets must be of the same type.")
-        if len(set(kets)) != len(kets):
-            raise ValueError("RydbergStateMQDT initialized with duplicate kets.")
+        if len(coefficients) != len(sqdt_states):
+            raise ValueError("Length of coefficients and sqdt_states must be the same.")
+        if not all(type(sqdt_state) is type(sqdt_states[0]) for sqdt_state in sqdt_states):
+            raise ValueError("All sqdt_states must be of the same type.")
+        if len(set(sqdt_states)) != len(sqdt_states):
+            raise ValueError("RydbergStateMQDT initialized with duplicate sqdt_states.")
         if abs(self.norm - 1) > 1e-10 and warn_if_not_normalized:
             logger.warning(
-                "RydbergStateMQDT initialized with non-normalized coefficients (norm=%s, coefficients=%s, kets=%s)",
+                "RydbergStateMQDT initialized with non-normalized coefficients (norm=%s, coefficients=%s, sqdt_states=%s)",
                 self.norm,
                 coefficients,
-                kets,
+                sqdt_states,
             )
         if self.norm > 1:
             self.coefficients /= self.norm
 
     def __iter__(self) -> Iterator[tuple[float, _RydbergState]]:
-        return zip(self.coefficients, self.kets).__iter__()
+        return zip(self.coefficients, self.sqdt_states).__iter__()
 
     def __repr__(self) -> str:
-        terms = [f"{coeff}*{ket!r}" for coeff, ket in self]
+        terms = [f"{coeff}*{sqdt_state!r}" for coeff, sqdt_state in self]
         return f"{self.__class__.__name__}({', '.join(terms)})"
 
     def __str__(self) -> str:
-        terms = [f"{coeff}*{ket!s}" for coeff, ket in self]
+        terms = [f"{coeff}*{sqdt_state!s}" for coeff, sqdt_state in self]
         return f"{', '.join(terms)}"
 
     @property
@@ -64,7 +70,7 @@ class RydbergStateMQDT(Generic[_RydbergState]):
     @property
     def angular(self) -> AngularState[Any]:
         """Return the angular part of the MQDT state as an AngularState."""
-        angular_kets = [ket.angular for ket in self.kets]
+        angular_kets = [ket.angular for ket in self.sqdt_states]
         return AngularState(self.coefficients.tolist(), angular_kets)
 
     def calc_reduced_overlap(self, other: RydbergStateMQDT[Any] | RydbergStateSQDT) -> float:
@@ -73,9 +79,9 @@ class RydbergStateMQDT(Generic[_RydbergState]):
             other = RydbergStateMQDT([1.0], [other])
 
         ov = 0
-        for coeff1, ket1 in self:
-            for coeff2, ket2 in other:
-                ov += np.conjugate(coeff1) * coeff2 * ket1.calc_reduced_overlap(ket2)
+        for coeff1, sqdt1 in self:
+            for coeff2, sqdt2 in other:
+                ov += np.conjugate(coeff1) * coeff2 * sqdt1.calc_reduced_overlap(sqdt2)
         return ov
 
     def calc_matrix_element(
@@ -93,7 +99,7 @@ class RydbergStateMQDT(Generic[_RydbergState]):
             other = RydbergStateMQDT([1.0], [other])
 
         value = 0
-        for coeff1, ket1 in self:
-            for coeff2, ket2 in other:
-                value += np.conjugate(coeff1) * coeff2 * ket1.calc_matrix_element(ket2, operator, kappa)
+        for coeff1, sqdt1 in self:
+            for coeff2, sqdt2 in other:
+                value += np.conjugate(coeff1) * coeff2 * sqdt1.calc_matrix_element(sqdt2, operator, kappa)
         return value
