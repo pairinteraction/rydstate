7368b625f0462ff4727749c198f125bf47705f1a
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-12-05 16:49:58 +0100

    add BasisSQDTAlkalineJJ and BasisSQDTAlkalineFJ

--- src/rydstate/basis/basis_sqdt.py
+++ src/rydstate/basis/basis_sqdt.py
@@ -3,7 +3,12 @@ from __future__ import annotations
 import numpy as np
 
 from rydstate.basis.basis_base import BasisBase
-from rydstate.rydberg import RydbergStateSQDTAlkali, RydbergStateSQDTAlkalineLS
+from rydstate.rydberg import (
+    RydbergStateSQDTAlkali,
+    RydbergStateSQDTAlkalineFJ,
+    RydbergStateSQDTAlkalineJJ,
+    RydbergStateSQDTAlkalineLS,
+)
 
 
 class BasisSQDTAlkali(BasisBase[RydbergStateSQDTAlkali]):
@@ -42,5 +47,49 @@ class BasisSQDTAlkalineLS(BasisBase[RydbergStateSQDTAlkalineLS]):
                         for f_tot in np.arange(abs(j_tot - i_c), j_tot + i_c + 1):
                             state = RydbergStateSQDTAlkalineLS(
                                 species, n=n, l=l, s_tot=s_tot, j_tot=j_tot, f_tot=float(f_tot)
+                            )
+                            self.states.append(state)
+
+
+class BasisSQDTAlkalineJJ(BasisBase[RydbergStateSQDTAlkalineJJ]):
+    def __init__(self, species: str, n_min: int = 0, n_max: int | None = None) -> None:
+        super().__init__(species)
+
+        if n_max is None:
+            raise ValueError("n_max must be given")
+
+        i_c = self.species.i_c if self.species.i_c is not None else 0
+        j_c = 0.5
+        s_r = 0.5
+        self.states = []
+        for n in range(n_min, n_max + 1):
+            for l_r in range(n):
+                for j_r in np.arange(abs(l_r - s_r), l_r + s_r + 1):
+                    for j_tot in range(int(abs(j_r - j_c)), int(j_r + j_c + 1)):
+                        for f_tot in np.arange(abs(j_tot - i_c), j_tot + i_c + 1):
+                            state = RydbergStateSQDTAlkalineJJ(
+                                species, n=n, l=l_r, j_r=float(j_r), j_tot=j_tot, f_tot=float(f_tot)
+                            )
+                            self.states.append(state)
+
+
+class BasisSQDTAlkalineFJ(BasisBase[RydbergStateSQDTAlkalineFJ]):
+    def __init__(self, species: str, n_min: int = 0, n_max: int | None = None) -> None:
+        super().__init__(species)
+
+        if n_max is None:
+            raise ValueError("n_max must be given")
+
+        i_c = self.species.i_c if self.species.i_c is not None else 0
+        j_c = 0.5
+        s_r = 0.5
+        self.states = []
+        for n in range(n_min, n_max + 1):
+            for f_c in np.arange(abs(j_c - i_c), j_c + i_c + 1):
+                for l_r in range(n):
+                    for j_r in np.arange(abs(l_r - s_r), l_r + s_r + 1):
+                        for f_tot in np.arange(abs(f_c - j_r), f_c + j_r + 1):
+                            state = RydbergStateSQDTAlkalineFJ(
+                                species, n=n, l=l_r, j_r=float(j_r), f_c=float(f_c), f_tot=float(f_tot)
                             )
                             self.states.append(state)
