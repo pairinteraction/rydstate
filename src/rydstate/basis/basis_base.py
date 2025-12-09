da4923af07fcc03e6cf9fa9bd560653d9674685b
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-12-09 15:36:13 +0100

    basis add sort_states

--- src/rydstate/basis/basis_base.py
+++ src/rydstate/basis/basis_base.py
@@ -57,6 +57,17 @@ class BasisBase(ABC, Generic[_RydbergState]):
 
         return self
 
+    def sort_states(self, *qns: str) -> Self:
+        """Sort the basis states according to the given quantum numbers.
+
+        The first quantum number given is the primary sorting key, the second quantum number
+        is the secondary sorting key, and so on.
+        """
+        values = np.array([self.calc_exp_qn(qn) for qn in qns])
+        sorted_indices = np.lexsort(values[::-1])
+        self.states = [self.states[i] for i in sorted_indices]
+        return self
+
     def calc_exp_qn(self, qn: str) -> list[float]:
         if is_angular_momentum_quantum_number(qn):
             return [state.angular.calc_exp_qn(qn) for state in self.states]
