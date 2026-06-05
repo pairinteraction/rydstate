58b6484d08842a3827740edc7e447c204193d1bd
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2026-06-05 10:04:42 +0200

    mqdt data dont define i_c

--- src/rydstate/species/strontium/mqdt_data.py
+++ src/rydstate/species/strontium/mqdt_data.py
@@ -11,13 +11,12 @@ from rydstate.species.strontium import sr87_mqdt_fmodel_data, sr88_mqdt_fmodel_d
 class MQDTStrontium87(MQDT):
     species = "Sr87"
     is_default = True
-    i_c = 4.5
 
     ionization_threshold_dict: ClassVar = {
-        CoreKet(i_c, 0.5, 0, 0.5, 4): (45932.287373577, None, "1/cm"),
-        CoreKet(i_c, 0.5, 0, 0.5, 5): (45932.120512528, None, "1/cm"),
+        CoreKet(4.5, 0.5, 0, 0.5, 4): (45932.287373577, None, "1/cm"),
+        CoreKet(4.5, 0.5, 0, 0.5, 5): (45932.120512528, None, "1/cm"),
     }
-    core_ground_state = CoreKet(i_c, 0.5, 0, 0.5, 4)
+    core_ground_state = CoreKet(4.5, 0.5, 0, 0.5, 4)
     nuclear_dipole = -1.0936030
     model_classes = get_fmodels(sr87_mqdt_fmodel_data, species)
 
@@ -25,11 +24,10 @@ class MQDTStrontium87(MQDT):
 class MQDTStrontium88(MQDT):
     species = "Sr88"
     is_default = True
-    i_c = 0
 
     ionization_threshold_dict: ClassVar = {
-        CoreKet(i_c, 0.5, 0, 0.5): (45932.1956, None, "1/cm"),
+        CoreKet(0, 0.5, 0, 0.5): (45932.1956, None, "1/cm"),
     }
-    core_ground_state = CoreKet(i_c, 0.5, 0, 0.5)
+    core_ground_state = CoreKet(0, 0.5, 0, 0.5)
     nuclear_dipole = 2.3
     model_classes = get_fmodels(sr88_mqdt_fmodel_data, species)
